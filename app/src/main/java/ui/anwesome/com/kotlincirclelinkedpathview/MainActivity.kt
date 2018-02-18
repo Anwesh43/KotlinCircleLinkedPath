3b5cc69342c0b6724c8d28f76495e20b38b4cac5
Anwesh43 <anweshthecool0@gmail.com>
2018-02-18 22:16:23 +0530

    adding fullscreen support

--- app/src/main/java/ui/anwesome/com/kotlincirclelinkedpathview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlincirclelinkedpathview/MainActivity.kt
@@ -1,7 +1,9 @@
 package ui.anwesome.com.kotlincirclelinkedpathview
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import ui.anwesome.com.circlelinkedpathview.CircleLinkedPathView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val view = CircleLinkedPathView.create(this)
+        fullScreen()
     }
+}
+fun AppCompatActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
