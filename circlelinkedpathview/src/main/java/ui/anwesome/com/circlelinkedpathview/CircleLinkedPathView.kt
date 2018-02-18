dcf9ce4d58d5840d28b979dc96d476d6b7bea94f
Anwesh43 <anweshthecool0@gmail.com>
2018-02-18 11:41:05 +0530

    adding CircleNode class

--- circlelinkedpathview/src/main/java/ui/anwesome/com/circlelinkedpathview/CircleLinkedPathView.kt
+++ circlelinkedpathview/src/main/java/ui/anwesome/com/circlelinkedpathview/CircleLinkedPathView.kt
@@ -64,4 +64,36 @@ class CircleLinkedPathView(ctx:Context):View(ctx) {
             }
         }
     }
+    data class CircleNode(var i:Int, var deg:Float, var r:Float) {
+        val x:Float = r*Math.cos(i*Math.PI/180*deg).toFloat()
+        val y:Float = r*Math.sin(i*Math.PI/180*deg).toFloat()
+        var neighbor:CircleNode ?= null
+        val state = State()
+        fun addNeighbor(circleNode: CircleNode) {
+            neighbor = circleNode
+        }
+        fun draw(canvas:Canvas, paint:Paint, var curr:Boolean) {
+            val scale1 = state.scales[0]
+            val scale2 = state.scales[1]
+            paint.style = Paint.Style.STROKE
+            canvas.drawCircle(x, y, r/15, paint)
+            if(curr) {
+                paint.style = Paint.Style.FILL
+                canvas.drawCircle(x, y, (r / 15) * (1 - scale1), paint)
+                val x1 = neighbor?.x ?: 0f
+                val y1 = neighbor?.x ?: 0f
+                val updatePoints: (Float) -> PointF = { scale -> PointF(x + (x1 - x)*scale, y +(y1 - y)*scale) }
+                val point1 = updatePoints(scale1)
+                val point2 = updatePoints(scale2)
+                canvas.drawLine(point2.x , point2.y ,point1.x, point1.y, paint)
+                canvas.drawCircle(x1, y1, r/15*scale2, paint)
+            }
+        }
+        fun update(stopcb: (Float) -> Unit) {
+            state.update(stopcb)
+        }
+        fun startUpdating(startcb : () -> Unit) {
+            state.startUpdating(startcb)
+        }
+    }
 }
